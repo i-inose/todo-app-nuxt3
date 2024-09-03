073122331a256f8d3197731a52f1d3e665a0eed0
i-inose <i.inose0304@gmail.com>
2024-09-04 00:17:01 +0900

    Initial commit

---
+++ components/TaskInput.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue"
+
+const emit = defineEmits(["add-task"])
+const newTask = ref("")
+
+const emitAddTask = () => {
+  if (newTask.value.trim()) {
+    emit('add-task', newTask.value.trim())
+    newTask.value = ""
+  }
+}
+</script>
+
+<template>
+	<div>
+		<input v-model="newTask" placeholder="タスクを入力してください"/>
+		<button @click="emitAddTask">追加</button>
+	</div>
+</template>
---
+++ components/TaskList.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+	tasks: Array
+})
+const emit = defineEmits(["remove-task"])
+const emitRemoveTask = (index) => {
+	emit("remove-task", index)
+}
+</script>
+
+<template>
+	<ul>
+		<li v-for="(task, index) in tasks" :key="index">
+			{{ task }}
+			<button @click="emitRemoveTask(index)">削除</button>
+		</li>
+	</ul>
+</template>

97b13f1d2f015957ef9c96a3e3077a7313a38cd9
i-inose <i.inose0304@gmail.com>
2024-09-04 01:43:20 +0900

    ログイン機能の追加

---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<script setup>
+	const { status, data, signIn } = useAuth();
+	definePageMeta({
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: "/",
+  },
+});
+</script>
+
+<template>
+	<div>
+		<h1>ログイン</h1>
+		<button @click="signIn('github')" type="submit">Githubでログイン</button>
+	</div>
+</template>

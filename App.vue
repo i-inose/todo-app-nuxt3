5dd6c139d0cd13713c0bc9685c6007c3f030a3f5
i-inose <i.inose0304@gmail.com>
2024-09-06 22:50:35 +0900

    修正

--- App.vue
+++ App.vue
@@ -1,44 +1,9 @@
-<script setup>
-import { ref } from "vue"
-import TaskInput from "./components/TaskInput.vue"
-import TaskList from "./components/TaskList.vue"
+<script>
 
-const tasks = ref([])
-
-const loadTasks = () => {
-  const savedTasks = localStorage.getItem("tasks")
-  if (savedTasks) {
-    tasks.value = JSON.parse(savedTasks)
-  }
-}
-
-const saveTasks = () => {
-  localStorage.setItem("tasks", JSON.stringify(tasks.value))
-}
-
-onMounted(() => {
-  loadTasks()
-})
-
-const addTask = (task) => {
-  tasks.value.push(task)
-  saveTasks()
-}
-
-const removeTask = (index) => {
-  tasks.value.splice(index, 1)
-}
 </script>
 
 <template>
-  <div>
-    <h1>ToDoリスト</h1>
-    <hr />
-    <div class="inner-container">
-      <TaskInput @add-task="addTask" />
-      <TaskList :tasks="tasks" @remove-Task="removeTask" />
-    </div>
-  </div>
+    <NuxtPage />
 </template>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,42 @@
 <script setup>
-import App from '../App.vue';
+import { ref } from "vue"
+import TaskInput from "../components/TaskInput.vue"
+import TaskList from "../components/TaskList.vue"
+
+const tasks = ref([])
+
+const loadTasks = () => {
+  const savedTasks = localStorage.getItem("tasks")
+  if (savedTasks) {
+    tasks.value = JSON.parse(savedTasks)
+  }
+}
+
+const saveTasks = () => {
+  localStorage.setItem("tasks", JSON.stringify(tasks.value))
+}
+
+onMounted(() => {
+  loadTasks()
+})
+
+const addTask = (task) => {
+  tasks.value.push(task)
+  saveTasks()
+}
+
+const removeTask = (index) => {
+  tasks.value.splice(index, 1)
+}
 </script>
 
 <template>
-  <App />
+  <div>
+    <h1>ToDoリスト</h1>
+    <hr />
+    <div class="inner-container">
+      <TaskInput @add-task="addTask" />
+      <TaskList :tasks="tasks" @remove-task="removeTask" />
+    </div>
+  </div>
 </template>
